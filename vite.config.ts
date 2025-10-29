9566a0c6a77cf66730ebbd7cbf4c6221811e08bf
Salaman <salaman.khan@itrtechsystems.com>
2025-10-30 00:22:21 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     allowedHosts: [
-      "https://construction-ui-stg.addisababadbohra.com", // 👈 your staging domain
+      "construction-ui-stg.addisababadbohra.com", // 👈 your staging domain
       ".addisabadbohra.com", // optional wildcard for subdomains
       "localhost",
       "127.0.0.1",
